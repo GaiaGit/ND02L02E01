9cc6e7e2ab8abda1ab57d0d22ce894afe2f86e69
GaiaGit <geacassay@gmail.com>
2018-08-21 21:10:38 +0000

    Updated Comment

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import logo from './logo.svg';
 
-/*
-Use React and the data below to display a list of users alongside their favorite movies.
-
-For detailed instructions, refer to instructions.md.
-*/
+/*Use React and the data below to display a list of users alongside their favorite movies.*/
 
 const profiles = [
   {
